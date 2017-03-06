cef6ef67b8e211250543f661f74849b252156602
marisid <sid_marina@hotmail.com>
2017-03-06 18:38:04 +0000

    Tidy up index.js and make http requests from front-end

--- public/index.js
+++ public/index.js
@@ -1,34 +1,29 @@
 (function() {
 
-  // const message = document.getElementsByClassName('message')[0];
-  // const red = document.getElementsByClassName('red')[0];
-  // const blue = document.getElementsByClassName('blue')[0];
-  // const yellow = document.getElementsByClassName('yellow')[0];
-
+  const redBtn = document.getElementById('red-button');
+  const blueBtn = document.getElementById('blue-button');
+  const yellowBtn = document.getElementById('yellow-button');
   const body = document.querySelector('body');
 
-  body.style.backgroundColor = "cyan";
+  var httpGetRequest = (colour) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        body.style.backgroundColor = colour;
+      }
+    };
+    xhr.open('GET', '/', true);
+    xhr.send();
+  };
 
-  // httpGetRequest = (colour) => {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.onreadystatechange = () => {
-  //     if (xhr.readyState == 4 && xhr.status == 200) {
-  //       body.style.backgroundColor = colour;
-  //       message.textContent = xhr.responseText;
-  //     }
-  //   };
-  //   xhr.open('GET', , true);
-  //   xhr.send();
-  // };
-  //
-  // red.addEventListener('click', () => {
-  //   httpGetRequest('red');
-  // });
-  // blue.addEventListener('click', () => {
-  //   httpGetRequest('blue');
-  // });
-  // yellow.addEventListener('click', () => {
-  //   httpGetRequest('yellow');
-  // });
+  redBtn.addEventListener('click', () => {
+    httpGetRequest('red');
+  });
+  blueBtn.addEventListener('click', () => {
+    httpGetRequest('blue');
+  });
+  yellowBtn.addEventListener('click', () => {
+    httpGetRequest('yellow');
+  });
 
 })();
