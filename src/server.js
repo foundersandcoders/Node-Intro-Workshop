7b79cd88cdb45eb990aa33f25bd6d7354f952473
marisid <sid_marina@hotmail.com>
2017-03-12 15:33:18 +0000

    Refactor handler in server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 var port = 4000;
 
-var server = http.createServer(function(request, response) {
+var handler = function(request, response) {
   var url = request.url;
   if (url === '/') {
     fs.readFile(path.join(__dirname, '..', 'index.html'), function(err, file) {
@@ -13,24 +13,17 @@ var server = http.createServer(function(request, response) {
       response.end(file);
     });
   }
-  else if (url === '/public/main.css') {
-    fs.readFile(path.join(__dirname, '..', 'public', 'main.css'), function(err, file) {
+  else if (url.indexOf('public') !== -1) {
+    var extension = url.split('.')[1];
+    var extensionType = {
+      'html': 'text/html',
+      'css': 'text/css',
+      'js': 'application/javascript',
+      'ico': 'image/x-icon'
+    };
+    fs.readFile(path.join(__dirname, '..', url), function(err, file) {
       if (err) throw err;
-      response.writeHead(200, {'Content-Type': 'text/css'});
-      response.end(file);
-    });
-  }
-  else if (url === '/public/index.js') {
-    fs.readFile(path.join(__dirname, '..', 'public', 'index.js'), function(err, file) {
-      if (err) throw err;
-      response.writeHead(200, {'Content-Type': 'application/javascript'});
-      response.end(file);
-    });
-  }
-  else if (url === '/public/node-icon.ico') {
-    fs.readFile(path.join(__dirname, '..', 'public', 'node-icon.ico'), function(err, file) {
-      if (err) throw err;
-      response.writeHead(200, {'Content-Type': 'image/x-icon'});
+      response.writeHead(200, {'Content-Type': extensionType[extension]});
       response.end(file);
     });
   }
@@ -38,7 +31,9 @@ var server = http.createServer(function(request, response) {
     response.writeHead(404,  {'Content-Type': 'text/html'});
     response.end('<h1> 404: Page Requested Not Found </h1>');
   }
-});
+}
+
+var server = http.createServer(handler);
 
 server.listen(port);
 
