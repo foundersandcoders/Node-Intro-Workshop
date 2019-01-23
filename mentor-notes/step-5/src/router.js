8b86c9f8f4150ba9785ed4365fd61ec23b61ba1e
ali <ali.7@outlook.com>
2019-01-23 06:25:05 +0200

    modify-Content-Type-headers-to-object-in-step-5-router relates #21

--- mentor-notes/step-5/src/router.js
+++ mentor-notes/step-5/src/router.js
@@ -7,7 +7,7 @@ const router = (request, response) => {
   } else if (url.indexOf('public') !== -1) {
     handlers.handlePublic(request, response, url);
   } else {
-    response.writeHead(404, 'Content-Type: text/html');
+    response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end('<h1>404 file not found</h1>');
   }
 };
