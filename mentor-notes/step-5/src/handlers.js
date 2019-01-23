101c55609a5196064d0d1c3adf2c34851f3c8f93
ali <ali.7@outlook.com>
2019-01-23 06:25:46 +0200

    modify-Content-Type-headers-to-object-in-step-5-handlers relates #21

--- mentor-notes/step-5/src/handlers.js
+++ mentor-notes/step-5/src/handlers.js
@@ -6,10 +6,10 @@ const handleHomeRoute = (request, response) => {
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(500, 'Content-Type: text/html');
+      response.writeHead(500, { 'Content-Type': 'text/html' });
       response.end("<h1>Sorry, we've had a problem on our end</h1>");
     } else {
-      response.writeHead(200, 'Content-Type: text/html');
+      response.writeHead(200, { 'Content-Type': 'text/html' });
       response.end(file);
     }
   });
@@ -29,10 +29,10 @@ const handlePublic = (request, response, url) => {
   fs.readFile(filePath, (error, file) => {
     if (error) {
       console.log(error);
-      response.writeHead(404, 'Content-Type: text/html');
+      response.writeHead(404, { 'Content-Type': 'text/html' });
       response.end('<h1>404 file not found</h1>');
     } else {
-      response.writeHead(200, `Content-Type: ${extensionType[extension]}`);
+      response.writeHead(200, { 'Content-Type': extensionType[extension] });
       response.end(file);
     }
   });
