e35ab855a29621a9b9b82da4bac574afb6fd422f
ali <ali.7@outlook.com>
2019-01-23 06:23:09 +0200

    modify-Content-Type-headers-to-object-in-step-2.5 relates #21

--- mentor-notes/step-2.5/src/router.js
+++ mentor-notes/step-2.5/src/router.js
@@ -8,17 +8,17 @@ const router = (request, response) => {
     fs.readFile(filePath, (error, file) => {
       if (error) {
         console.log(error);
-        response.writeHead(500, 'Content-Type: text/html');
+        response.writeHead(500, { 'Content-Type': 'text/html' });
         response.end("<h1>Sorry, we've had a problem on our end</h1>");
       } else {
-        response.writeHead(200, 'Content-Type: text/html');
+        response.writeHead(200, { 'Content-Type': 'text/html' });
         response.end(file);
       }
     });
   } else if (url === '/yolo') {
     response.end('this is yolo');
   } else {
-    response.writeHead(404, 'Content-Type: text/html');
+    response.writeHead(404, { 'Content-Type': 'text/html' });
     response.end('<h1>404 not found</h1>');
   }
 };
